ee3a1af8fee7fb8e1663ea484493c2a2f2ace16f
adriaanbotha <adriaan.botha@gmail.com>
2021-07-17 12:12:38 +1000

    Initial Swagger comit

---
+++ controllers/Public.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var utils = require('../utils/writer.js');
+var Public = require('../service/PublicService');
+
+module.exports.sendMail = function sendMail (req, res, next) {
+  var emailBody = req.swagger.params['emailBody'].value;
+  Public.sendMail(emailBody)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
